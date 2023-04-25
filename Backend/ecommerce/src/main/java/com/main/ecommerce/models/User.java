4b0dbf70b8666a0b73a4ebf9d3fa9bb87161d2ff
Gafencu Beniamin <benigafencu@yahoo.com>
2023-04-25 14:42:44 +0300

    Initial models of User and Role

---
+++ Backend/ecommerce/src/main/java/com/main/ecommerce/models/User.java
@@ -0,0 +1,23 @@
+package com.main.ecommerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity @Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+}
