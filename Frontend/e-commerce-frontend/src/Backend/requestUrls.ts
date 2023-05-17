de0845cb212ee1055ca7d0e5ae24a86bae3610e9
Gafencu Beniamin <benigafencu@yahoo.com>
2023-05-17 21:46:45 +0300

    Remove copied url's

--- Frontend/e-commerce-frontend/src/Backend/requestUrls.ts
+++ Frontend/e-commerce-frontend/src/Backend/requestUrls.ts
@@ -4,21 +4,6 @@ const authRouteBase = 'auth';
 const id = ':id';
 
 export const requestUrls = {
-    brands: `${backendEnvironment}${routeBase}/brands`,
-    models: `${backendEnvironment}${routeBase}/models`,
-
-    cars: `${backendEnvironment}${routeBase}/cars`,
-    car: `${backendEnvironment}${routeBase}/cars/${id}`,
-
-    cities: `${backendEnvironment}${routeBase}/cities`,
-    city: `${backendEnvironment}${routeBase}/cities/${id}`,
-
-    carWashStations: `${backendEnvironment}${routeBase}/car-wash-stations`,
-    carWashStation: `${backendEnvironment}${routeBase}/car-wash-stations/${id}`,
-
-    prices: `${backendEnvironment}${routeBase}/prices`,
-    gasStations: `${backendEnvironment}${routeBase}/gas-stations`,
-    carServices: `${backendEnvironment}${routeBase}/car-services`,
 
     authRegister: `${backendEnvironment}${routeBase}/${authRouteBase}/register`,
     authLogin: `${backendEnvironment}${routeBase}/${authRouteBase}/login`,
